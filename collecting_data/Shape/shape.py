20ac260d7cb7f3d6e59d22300ac4866454151b25
edufurlani <89416741+edufurlani@users.noreply.github.com>
2023-06-13 20:41:15 -0300

    Add files via upload

--- collecting_data/Shape/shape.py
+++ collecting_data/Shape/shape.py
@@ -29,8 +29,8 @@ def run(playwright: Playwright) -> None:
         page.query_selector(f'//*[@id="mntl-card-list-items_2-0-{x}"]/div[2]/span').click()
         try:
             texto = page.locator('//*[@id="article-content_1-0"]').text_content(timeout=15000)
-            with open('shape.txt', 'w', encoding='utf-8') as f:
-                f.write('Artigo 1\n')
+            with open('shape.txt', 'a', encoding='utf-8') as f:
+                f.write(f'Artigo {x}\n')
                 f.write(texto)
 
                 page.goto("https://www.shape.com/search?q=whey+protein")
