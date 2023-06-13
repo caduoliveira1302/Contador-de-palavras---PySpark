b26ee7f86ad5a6c22c20d380276e15c8f3776f96
edufurlani <89416741+edufurlani@users.noreply.github.com>
2023-06-13 16:32:47 -0300

    Add files via upload

--- collecting_data/lifestrong/livestrong.py
+++ collecting_data/lifestrong/livestrong.py
@@ -1,6 +1,5 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 import time
-import csv
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
@@ -24,11 +23,10 @@ def run(playwright: Playwright) -> None:
 
     # Crio o csv 'livestrong.csv'
 
-    with open('livestrong.csv', 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Artigo', 'Texto'])
-        writer.writerow(['Artigo 1', texto])
-    
+    with open('livestrong.txt', 'w', encoding='utf-8') as f:
+        f.write('Artigo 1\n')
+        f.write(texto)
+
     #Crio um laço de repetição para entrar e adicionar as informações no csv livestrong.csv
     
     for x in range(2,11):
@@ -38,9 +36,9 @@ def run(playwright: Playwright) -> None:
         page.query_selector(f'//*[@id="app"]/div[2]/main/div/div/div[2]/div[2]/div[{x}]').click()
         texto = page.locator('//*[@id="app"]/div[2]/main/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/article').text_content()
 
-        with open('livestrong.csv', 'a', newline='', encoding='utf-8') as f:
-                writer = csv.writer(f)
-                writer.writerow([f'Artigo {x}', texto])
+        with open('livestrong.txt', 'a', encoding='utf-8') as f:
+            f.write(f'Artigo {x}\n')
+            f.write(texto)
 
 
     # ---------------------
