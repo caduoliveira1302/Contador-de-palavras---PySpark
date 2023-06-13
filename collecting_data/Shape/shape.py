e56275d103dfb654d82d65dce895ad813362fe61
edufurlani <89416741+edufurlani@users.noreply.github.com>
2023-06-13 16:30:22 -0300

    Add files via upload

---
+++ collecting_data/Shape/shape.py
@@ -0,0 +1,51 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+import time
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.shape.com/search?q=whey+protein")
+
+    # Acessa o primeiro artigo
+
+    page.query_selector('//*[@id="mntl-card-list-items_2-0"]/div[2]/span').click()
+    time.sleep(5)
+    texto = page.locator('//*[@id="article-content_1-0"]').text_content()
+
+    # Cria e adiciona o primeiro artigo ao csv
+
+    with open('shape.txt', 'w', encoding='utf-8') as f:
+        f.write('Artigo 1\n')
+        f.write(texto)
+    page.goto("https://www.shape.com/search?q=whey+protein")
+    
+    # Acessa outros artigos
+    
+    for x in range(1,24):
+
+        page.goto("https://www.shape.com/search?q=whey+protein")
+        page.query_selector(f'//*[@id="mntl-card-list-items_2-0-{x}"]/div[2]/span').click()
+        try:
+            texto = page.locator('//*[@id="article-content_1-0"]').text_content(timeout=15000)
+            with open('shape.txt', 'w', encoding='utf-8') as f:
+                f.write('Artigo 1\n')
+                f.write(texto)
+
+                page.goto("https://www.shape.com/search?q=whey+protein")
+        except:
+             pass
+
+    
+
+
+
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
